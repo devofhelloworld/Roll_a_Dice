3de70c89d47fe340db04db51dee80112fd4ee4cd
devofhelloworld <sujaykumarsingh6228@gmail.com>
2025-06-03 02:20:23 +0530

    JavaScript Arrays Concept

---
+++ app.js
@@ -0,0 +1,32 @@
+const result = document.getElementById("results");
+const image = document.getElementById("diceimage");
+let number;
+let results = [];
+let img = [];
+
+function roll() {
+
+let values = document.getElementById("noOfDice").value;
+if(values<=10 && values>0){
+
+
+    for (let i = 0; i < values; i++) {
+
+        number = Math.ceil(Math.random() * 6);
+        results.push(number); 
+        img.push(`<img src="${number}.png">`);
+    }
+
+    result.textContent = `${values} Dice : ` + results.join(",");
+    image.innerHTML = img.join("");
+    
+}
+
+else{
+    image.innerHTML ="";
+    result.textContent = "Invalid Input. Input range allowed is [1,10]";
+}
+
+}
+
+
